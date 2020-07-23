00f72b298870887b04392f091e36231be367675f
whatYnext <63004699+whatYnext@users.noreply.github.com>
2020-07-23 13:47:15 +0200

    Add files via upload

---
+++ Recommender systems/Matrix factorization.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Implementation of matrix factorization for dataset: MovieLens
+Highlights:
+    1. Matrix factorization as R=P*Q^T ((m,k) times (k,n))where P_u is interests of users;
+       and Q_v is the properties of items where k is the priority dimension
+    2. min ||R-\bar{R}||_2+\lambda(||P||+||Q||+||b_p||+||b_q||) where
+       b stands for bias for every user and item
+    3. Use Adam to solve
+    4. After getting P, Q, b_p,b_q, we can make prediction for unknown scores
+
+"""
+
+from d2l import mxnet as d2l
+from mxnet import autograd, gluon, np, npx
+from mxnet.gluon import nn
+import mxnet as mx
+npx.set_np()
+# Model implementation
+class MF(nn.Block):
+    def __init__(self, num_factors, num_users, num_items, **kwargs):
+        super(MF, self).__init__(**kwargs)
+        self.P = nn.Embedding(input_dim=num_users, output_dim=num_factors)
+        self.Q = nn.Embedding(input_dim=num_items, output_dim=num_factors)
+        self.user_bias = nn.Embedding(num_users, 1)
+        self.item_bias = nn.Embedding(num_items, 1)
+
+    def forward(self, user_id, item_id):
+        P_u = self.P(user_id)
+        Q_i = self.Q(item_id)
+        b_u = self.user_bias(user_id)
+        b_i = self.item_bias(item_id)
+        outputs = (P_u * Q_i).sum(axis=1) + np.squeeze(b_u) + np.squeeze(b_i)
+        return outputs.flatten()
+
+#Evaluation model using RMSE
+def evaluator(net, test_iter, ctx):
+    rmse = mx.metric.RMSE()  # Get the RMSE
+    rmse_list = []
+    for idx, (users, items, ratings) in enumerate(test_iter):
+        u = gluon.utils.split_and_load(users, ctx, even_split=False)
+        i = gluon.utils.split_and_load(items, ctx, even_split=False)
+        r_ui = gluon.utils.split_and_load(ratings, ctx, even_split=False)
+        r_hat = [net(u, i) for u, i in zip(u, i)]
+        rmse.update(labels=r_ui, preds=r_hat)
+        rmse_list.append(rmse.get()[1])
+    return float(np.mean(np.array(rmse_list)))
+
+#Train model with L2 loss and weight decay
+def train_recsys_rating(net, train_iter, test_iter, loss, trainer, num_epochs,
+                        ctx_list=d2l.try_all_gpus(), evaluator=None,
+                        **kwargs):
+    timer = d2l.Timer()
+    animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0, 2],
+                            legend=['train loss', 'test RMSE'])
+    for epoch in range(num_epochs):
+        metric, l = d2l.Accumulator(3), 0.
+        for i, values in enumerate(train_iter):
+            timer.start()
+            input_data = []
+            values = values if isinstance(values, list) else [values]
+            for v in values:
+                input_data.append(gluon.utils.split_and_load(v, ctx_list))
+            train_feat = input_data[0:-1] if len(values) > 1 else input_data
+            train_label = input_data[-1]
+            with autograd.record():
+                preds = [net(*t) for t in zip(*train_feat)]
+                ls = [loss(p, s) for p, s in zip(preds, train_label)]
+            [l.backward() for l in ls]
+            l += sum([l.asnumpy() for l in ls]).mean() / len(ctx_list)
+            trainer.step(values[0].shape[0])
+            metric.add(l, values[0].shape[0], values[0].size)
+            timer.stop()
+        if len(kwargs) > 0:  # It will be used in section AutoRec
+            test_rmse = evaluator(net, test_iter, kwargs['inter_mat'],
+                                  ctx_list)
+        else:
+            test_rmse = evaluator(net, test_iter, ctx_list)
+        train_l = l / (i + 1)
+        animator.add(epoch + 1, (train_l, test_rmse))
+    print(f'train loss {metric[0] / metric[1]:.3f}, '
+          f'test RMSE {test_rmse:.3f}')
+    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+          f'on {str(ctx_list)}')
+    
+ctx = d2l.try_all_gpus()
+num_users, num_items, train_iter, test_iter = d2l.split_and_load_ml100k(
+    test_ratio=0.1, batch_size=512)
+net = MF(30, num_users, num_items)
+net.initialize(ctx=ctx, force_reinit=True, init=mx.init.Normal(0.01))
+lr, num_epochs, wd, optimizer = 0.002, 20, 1e-5, 'adam'
+loss = gluon.loss.L2Loss()
+trainer = gluon.Trainer(net.collect_params(), optimizer,
+                        {"learning_rate": lr, 'wd': wd})
+train_recsys_rating(net, train_iter, test_iter, loss, trainer, num_epochs,
+                    ctx, evaluator)
+
+#Test one score for user:20's score on item 30
+scores = net(np.array([20], dtype='int', ctx=d2l.try_gpu()),
+             np.array([30], dtype='int', ctx=d2l.try_gpu()))
